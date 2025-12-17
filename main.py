2e03b5e034b9672001ea2d11507c061709f9a804
张17 <yq.zhang@worldelite.net.cn>
2025-12-17 15:02:06 +0800

    feat(test):确认详情页也可以用XPath爬取

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ if __name__ == '__main__':
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.0.0.0'
         }
         url = 'https://www.zhaopin.com/sou/jl765/kwE8M8CQO/p1'
-        response = requests.get(url, headers=headers)
+
+        # 接下来再验证一下详情页数据，是否也是可以直接通过发送请求获取到的
+        # 切换回浏览器，复制任何一个岗位详情页的URL，把他赋值给一个新变量url_detail,并替换掉requests.get里的参数
+        url_detail = 'https://www.zhaopin.com/jobdetail/CC000544460J40776127616.htm?refcode=4019&srccode=401903&preactionid=ec7bcbcd-def9-439f-8587-81f26684e0aa'
+        # response = requests.get(url, headers=headers)
+        response = requests.get(url_detail, headers=headers)
+        #运行程序后，在输出的HTML再次搜索，从结果可以看到，岗位详情页也可以用requests结合Xpath的方式抓取数据
         response.raise_for_status()
         print(response.text)
         #这就是网页服务器返回的HTML代码
@@ -23,3 +29,9 @@ if __name__ == '__main__':
     #那么第一步是安装依赖，在pycharm终端里运行安装命令pip install curl_cffi
     #等安装完成后回到代码，把原来的导入语句替换掉，不再import requests，而是用from curl_cffi import requests
     #再次运行，就能在返回的HTML里看到完整的页面结构了
+
+    #接着在终端里按快捷键Ctrl + F，打开搜索框，输入关键字“万”，用上下箭头切换查找结果，说明这些内容确实包含在HTML代码里
+    #接下来就可以用XPath或BS4解析列表页
+
+    #正式写代码之前，还可以做一个准备工作：就是把每个要爬取的数据项在网页上对应的位置找出来，记录到文档里
+    #这样到时候写XPath的时候，就不用在浏览器和代码编辑器上切换了
